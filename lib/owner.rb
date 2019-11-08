c7f7e74c3e145b46a49aa665a79b043121a7a973
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-08 15:29:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -76,7 +76,7 @@ class Owner
   end
   
     def list_pets
-        puts "I have #{self.dogs.count} dog(s), and #{@owner.cats.length} cat(s)."
+        puts "I have #{self.dogs.count} dog(s), and #{self.cats.length} cat(s)."
     end
   
   
