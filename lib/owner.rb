6cd26ee7557b46f8a25e402221dcd0e71257c243
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-08 14:56:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
   # code goes here
+  
+  attr_reader :species, :name
+  attr_accessor :cats
+  
+  @@all = []
+  @@pets = {:cats => []}
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    @@pets[:cats].collect do |cat|
+      cat.owner == self
+    end
+  end
+  
+  
 end
